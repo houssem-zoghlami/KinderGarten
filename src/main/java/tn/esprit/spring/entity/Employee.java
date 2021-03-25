b7452ba40a7cec7bdd928b284f956f90ce5f8e95
karimkhaldi <khaldi.karim@esprit.tn>
2021-03-25 14:14:14 +0100

    ajout entité employé

---
+++ src/main/java/tn/esprit/spring/entity/Employee.java
@@ -0,0 +1,44 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private int employee_id; 
+	@Enumerated(EnumType.STRING)
+	private Job job;
+	private String employeename;
+	private int age;
+  @ManyToOne
+  Kindergarten kindergarten;
+
+	
+}
