0382b41d0c28d924304a70ee2bb9ec445715538f
Marwen Gamha <57289527+marwenGamha@users.noreply.github.com>
2021-04-08 01:17:56 +0100

    rectification

---
+++ src/main/java/tn/esprit/spring/entity/Menu.java
@@ -0,0 +1,87 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Menu implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private int calories;
+	private String description;
+	
+	
+
+	
+	@OneToOne
+	Kindergarten kindergarten;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Menu(int id, String name, int calories, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+		this.description = description;
+	}
+
+	public Menu() {
+		super();
+	}
+	
+	// @Temporal(TemporalType.TIMESTAMP)
+	  //  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+		//private Date dateStart;
+	
+	
+
+}
